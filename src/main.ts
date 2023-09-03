74f30bacdfaa7f1bd757fe9886ef4343c3a83a62
Guy Indepurker <guy235@gmail.com>
2023-09-03 10:40:18 +0300

    fix vercel

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import cors from 'cors';
 
 import { router } from './routes';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 8080;
+const host = process?.env?.HOST ?? 'localhost';
+const port = process?.env?.PORT ? Number(process.env.PORT) : 8080;
 
 const app = express();
 
@@ -17,6 +17,8 @@ app.get('/health', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+export { app };
+
+// app.listen(port, host, () => {
+//   console.log(`[ ready ] http://${host}:${port}`);
+// });
