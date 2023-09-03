e222961d6c4930cccbcc7a3d86aee22514722944
Guy Indepurker <guy235@gmail.com>
2023-09-03 11:28:59 +0300

    add api routes

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ const port = process?.env?.PORT ? Number(process.env.PORT) : 8080;
 const app = express();
 
 app.use(cors());
-app.use(express.json({limit: '255mb'}));
+app.use(express.json({ limit: '255mb' }));
 
-app.use('/api',router);
+app.use('/api', router);
 
 app.get('/api/health', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
-export { app };
+export default app;
 
 // app.listen(port, host, () => {
 //   console.log(`[ ready ] http://${host}:${port}`);
